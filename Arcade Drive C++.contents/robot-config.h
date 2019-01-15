8f7f0203628ce160ad4c06e53ae4c0449c870479
andrei-zhukov <46249976+andrei-zhukov@users.noreply.github.com>
2019-01-14 21:18:41 -0500

    Ports reconfigured for new wiring setup.

--- Arcade Drive C++.contents/robot-config.h
+++ Arcade Drive C++.contents/robot-config.h
@@ -1,11 +1,11 @@
 using namespace vex;
 vex::brain Brain;
-vex::motor Motor11dl (vex::PORT1, vex::gearSetting::ratio18_1,false);
-vex::motor Motor01dl (vex::PORT2, vex::gearSetting::ratio18_1,false);
-vex::motor Motor03dl (vex::PORT3, vex::gearSetting::ratio18_1,false);
-vex::motor Motor04dr (vex::PORT11, vex::gearSetting::ratio18_1,true);
-vex::motor Motor16dr (vex::PORT12, vex::gearSetting::ratio18_1,true);
-vex::motor Motor02dr (vex::PORT13, vex::gearSetting::ratio18_1,true);
-vex::motor Motor05sp (vex::PORT14, vex::gearSetting::ratio6_1,false);
+vex::motor Motor04dr (vex::PORT1, vex::gearSetting::ratio18_1,true);
+vex::motor Motor16dr (vex::PORT2, vex::gearSetting::ratio18_1,true);
+vex::motor Motor01dl (vex::PORT9, vex::gearSetting::ratio18_1,false);
+vex::motor Motor11dl (vex::PORT10, vex::gearSetting::ratio18_1,false);
+vex::motor Motor05sp (vex::PORT11, vex::gearSetting::ratio6_1,false);
+vex::motor Motor02dr (vex::PORT12, vex::gearSetting::ratio18_1,true);
+vex::motor Motor03dl (vex::PORT20, vex::gearSetting::ratio18_1,false);
 vex::controller Controller1;
 vex::competition Competition;
